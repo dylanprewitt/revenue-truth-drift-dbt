470edee994454a22ad75893a7e1f00323de08599
dylanprewitt <dylanprewittcontact@gmail.com>
2026-03-03 19:53:58 -0800

    Update stg_salesforce_crm.sql

--- models/stg_salesforce_crm.sql
+++ models/stg_salesforce_crm.sql
@@ -1,7 +1,6 @@
-CREATE OR REPLACE VIEW `revenue-operations-analytics.raw.stg_salesforce_crm` AS
 SELECT
     customer_id,
     account_name,
     CAST(contract_value_arr AS NUMERIC) AS contract_value_arr,
     is_active AS crm_status
-FROM `revenue-operations-analytics.raw.salesforce_crm`;
+FROM `revenue-operations-analytics.raw.salesforce_crm`
