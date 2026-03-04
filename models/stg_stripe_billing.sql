93f50392fe9ff89500753c4b4b8b6e0317f84d78
dylanprewitt <dylanprewittcontact@gmail.com>
2026-03-03 19:53:38 -0800

    Update stg_stripe_billing.sql

--- models/stg_stripe_billing.sql
+++ models/stg_stripe_billing.sql
@@ -1,7 +1,6 @@
-CREATE OR REPLACE VIEW `revenue-operations-analytics.raw.stg_stripe_billing` AS
 SELECT
     customer_id,
     subscription_plan,
     CAST(mrr_amount AS NUMERIC) AS mrr_amount,
     status AS billing_status
-FROM `revenue-operations-analytics.raw.stripe_billing`;
+FROM `revenue-operations-analytics.raw.stripe_billing`
